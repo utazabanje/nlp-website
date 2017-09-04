ae4625e3c58554a4cb859f1f8096a20c24659e5b
Miodrag Piletic <utazabanje@hotmail.com>
2017-09-04 15:35:32 +0200

    Finish Reveal on scroll section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from "./modules/MobileMenu";
+import RevealOnScroll from "./modules/RevealOnScroll";
+import $ from "jquery";
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".my-work__box"), "85%");
+new RevealOnScroll($(".address__box"), "90%");
